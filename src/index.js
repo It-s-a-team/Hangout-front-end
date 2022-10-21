b427ee497384aa86d386defc838a4746ad382203
Jeffrey Smith <jeffrey_s_smith@live.com>
2022-10-21 01:03:40 -0700

    created the CSS for login page

--- src/index.js
+++ src/index.js
@@ -3,17 +3,25 @@ import ReactDOM from 'react-dom';
 // import auth0 dependencies
 import { Auth0Provider } from "@auth0/auth0-react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css'
 import App from './modules/App';
 
 
-
 ReactDOM.render(
+  
+  
   <Auth0Provider
+  
     domain={ process.env.REACT_APP_AUTH_DOMAIN }
     clientId={ process.env.REACT_APP_AUTH_CLIENT_ID }
     redirectUri={ process.env.REACT_APP_AUTH_REDIRECT_URI }
   >
+    
     <App />
+    
   </Auth0Provider>,
   document.getElementById('root')
+   
 );
+
+
---
+++ src/modules/Splash.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import LoginButton from './LoginButton'
+import '../css/Splash.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+export default class Splash extends Component {
+  render() {
+
+    return (
+     
+      <div className='maincontainer'>
+        <div class="container-fluid">
+          <div class="row no-gutter">             
+            <div class="col-md-6 d-none d-md-flex bg-image">
+            </div>
+            <div class="col-md-6 bg-light">
+              <div class="login d-flex align-items-center py-5">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-lg-10 col-xl-7 mx-auto">
+                      <h3 class="display-4">Login Page!</h3>
+                      <form>
+                        <div class="d-grid gap-2 mt-2">
+                          <LoginButton />
+                        </div>
+                          <div class="text-center d-flex justify-content-between mt-4"><p>Code by <a href="#" class="font-italic text-muted"> <u>Hangout Team</u></a></p></div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  };
+}
+    
