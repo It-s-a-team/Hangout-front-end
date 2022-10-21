b683948e2e7528607555ae7d14d7760f68736c60
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-10-21 16:19:10 +0000

    removed links from Splash and Footer

--- src/modules/Splash.js
+++ src/modules/Splash.js
@@ -5,14 +5,15 @@ import '../css/Splash.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-export default class Splash extends Component {
-  render() {
-
+export default class Splash extends Component
+{
+  render()
+  {
     return (
-     
+
       <div className='maincontainer'>
         <div class="container-fluid">
-          <div class="row no-gutter">             
+          <div class="row no-gutter">
             <div class="col-md-6 d-none d-md-flex bg-image">
             </div>
             <div class="col-md-6 bg-light">
@@ -20,13 +21,13 @@ export default class Splash extends Component {
                 <div class="container">
                   <div class="row">
                     <div class="col-lg-10 col-xl-7 mx-auto">
-                      <Header/>
+                      <Header />
                       <h3 class="display-4">Login Page!</h3>
                       <form>
                         <div class="d-grid gap-2 mt-2">
                           <LoginButton />
                         </div>
-                          <div class="text-center d-flex justify-content-between mt-4"><p>Code by Hangout Team</p></div>
+                        <div class="text-center d-flex justify-content-between mt-4"><p>Code by Hangout Team</p></div>
                       </form>
                     </div>
                   </div>
@@ -39,4 +40,3 @@ export default class Splash extends Component {
     )
   };
 }
-    
